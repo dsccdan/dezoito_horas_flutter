3e0309636b218db50dc650913f43c341cfe0607d
Daniel Caldas <daniel@dcaldas.rj.ip.tv>
2019-07-11 12:44:09 -0300

    dezoito_horas em flutter

---
+++ android/app/src/main/kotlin/br/com/dezoitohoras/dezoito_horas/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.dezoitohoras.dezoito_horas
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
